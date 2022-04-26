8b5c8a597a690987a7db3b14648eca83b31fbc62
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-04-26 20:08:40 +0530

    login popup ui done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,10 +10,32 @@ import {
   VideoCall,
 } from "@material-ui/icons";
 import logo from "../../assets/logo1.png";
+// import { useAppContext } from "../../context/appContext";
 
 import "./styles.css";
 
+const useStyles = makeStyles((theme) => ({
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
+
 const Header = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const classes = useStyles();
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
   return (
     <div className="header">
       <div className="header__left">
@@ -32,7 +54,71 @@ const Header = () => {
         <VideoCall />
         <Apps />
         <Notifications />
-        <Avatar />
+        <Avatar onClick={handleClick}/>
+
+        <Popover
+          open={open}
+          id={id}
+          onClose={handleClose}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+          }}
+        >
+          <div className="home__popoverContainer">
+            <div className="home__popover__top">
+              <Badge
+                overlap="circular"
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                badgeContent={
+                  <div className="home__badge">
+                    <CameraAltOutlined className="home__camera" />
+                  </div>
+                }
+              >
+                <Avatar className={classes.large} />
+              </Badge>
+
+              <div className="home__text">
+                <div className="home__displayName">user name
+                  {/* {currentUser?.displayName} */}
+                </div>
+                <div className="home__mail">email id
+                  {/* {currentUser?.email} */}
+                  </div>
+              </div>
+              <div className="home__btn">Manage your google account</div>
+            </div>
+
+            <div className="home__popover__btm">
+              <div className="home__addBtn">
+                <PersonAddOutlined className="home__addIcon" />
+                <p>Add another account</p>
+              </div>
+
+              <Button
+                // onClick={() => auth.signOut()}
+                variant="outlined"
+                className="home__signOut"
+              >
+                Sign Out
+              </Button>
+
+              <div className="home__popover__footer">
+                <p>Privacy Policy</p>
+                <span>•</span>
+                <p>Terms of service</p>
+              </div>
+            </div>
+          </div>
+        </Popover>
       </div>
     </div>
   );
